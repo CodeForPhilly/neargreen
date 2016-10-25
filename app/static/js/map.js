ef52d6bf98a59ea998df45682f91e3e76619be27
Brad Steinberg <steinbergbrad@gmail.com>
2016-10-25 00:52:00 -0400

    Some js changes for making the map slightly more interactive. added logo image and leaf marker image

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,9 +1,10 @@
 $(function() {
 	var map;
 	var startLat, startLng;
+	var infowindow;
 
 	function onLoad() {
-		var lat, lng;
+		
 		function getQueryVariable(variable)
 		{
 		       var query = window.location.search.substring(1);
@@ -25,23 +26,9 @@ $(function() {
 				if (status === "OK") {
 					startLat = results[0].geometry.location.lat();
 					startLng = results[0].geometry.location.lng();
+
+					initMap(startLat, startLng);
 				}
-
-				var pt = {
-					"type": "Feature",
-					"properties": {},
-					"geometry": {
-						"type": "Point",
-						"coordinates": [Number(startLng), Number(startLat)]
-					}
-				};
-
-				var stores = findClosestStores(pt);
-
-				if (stores.length > 0)
-					ko.applyBindings(new HealthyStoresViewModel(stores));
-
-				initMap(startLat, startLng);
 			});
 		}
 
@@ -54,32 +41,51 @@ $(function() {
 			lng: Number(lng)
 		};
 
+		var pt = {
+			"type": "Feature",
+			"properties": {},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [Number(lng), Number(lat)]
+			}
+		};
+
+		var stores = findClosestStores(pt);
+		var markers = [];
+
 		map = new google.maps.Map(document.getElementById('map'), {
 			center: latlng,
-			zoom: 16,
+			zoom: 14,
 			draggable: true
 		});
 
-		var marker = new google.maps.Marker({
-			position: latlng,
-			map: map,
-			title: 'Hello World!'
-		});
+		if (stores.length > 0)
+		{
+			if (stores.length > 5) {
+				$(".results-container").addClass("results-container-scrollable");
+			}
+
+			for (var i = 0; i < stores.length; i++) 
+			{
+				var store = stores[i];
+				store.marker = makeMarker(store.name, store.latlng);
+			}
+
+			ko.applyBindings(new HealthyStoresViewModel(stores));
+		}
 
 		$(".store-row").click(function() {
 			var self = this;
-			console.log(self);
 			//remove active state class to previous elements
 			$(".store-row").find(".panel-primary").removeClass("active");
 			var lat = $($(self).find("[data-lat]")[0]).attr("data-lat");
 			var lng = $($(self).find("[data-lng]")[0]).attr("data-lng");
-			console.log(lat)
+
+			map.panTo({lat: Number(lat), lng: Number(lng)});
 
 			//highlight what store has  been selected
 			var panel = $(this).find(".panel-primary");
 			$(panel).addClass("active");
-
-			initMap(lat, lng);
 		});
 
 	}
@@ -121,6 +127,32 @@ $(function() {
 		return stores;
 	}
 
+	function makeMarker(name, latlng)
+	{	
+		var marker = new google.maps.Marker({
+			position: {
+				lat: latlng[0],
+				lng: latlng[1]
+			},
+			map: map,
+			title: name,
+			icon: {
+				url: '../../../images/leaf.png',
+				scaledSize: new google.maps.Size(35.5, 52.25), // scaled size
+			    origin: new google.maps.Point(0,0), // origin
+			    anchor: new google.maps.Point(0, 0) // anchor
+			}
+		});
+
+		marker.addListener('click', function() {
+			infowindow = new google.maps.InfoWindow({
+			    content: '<h5>' + marker.title + '</h5>'
+			  });
+			infowindow.open(map, marker);
+		});
+
+		return marker;
+	}
 
 	function HealthyStoresViewModel(stores) {
 		var self = this;
