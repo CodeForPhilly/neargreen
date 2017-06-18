ceb7bc231e22247808cd8558273e924446162013
Brad Steinberg <steinbergbrad@gmail.com>
2017-06-18 12:51:04 -0400

    My first commit to NearGreen2017

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
+var express 		= require('express');
+var request			= require('request');
+var app				= express();
+
+app.get('/', function (req, res) {
+	res.send('Hello World!');
+});
+
+app.listen(3000, function() {
+	console.log("Listening on port 3000");
 });
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  items: FirebaseListObservable<any[]>;
+  constructor(af: AngularFire) {
+    this.items = af.database.list('/features');
+  }
+}
